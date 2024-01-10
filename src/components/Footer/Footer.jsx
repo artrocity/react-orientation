65635bb5d79f28c6b8261414afcc7b0ea6b016d3
Ryan Barfield <cadium828@gmail.com>
2024-01-09 19:35:56 -0600

    added the card and footer componenet

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+// Start all components with a capital letter for the function name
+function Footer() {
+    return(
+        // HTML Markup goes here
+        <p className="read-the-docs">
+            Click on the Vite and React logos to learn more. This is proving that it works!
+        </p>
+    );
+}
+
+// Export the footer function using the newer JS standard
+export default Footer;
