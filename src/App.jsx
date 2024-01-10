d5733843947244f0e42c8d81cdd82d53bbcae4ad
Ryan Barfield <cadium828@gmail.com>
2024-01-09 21:02:14 -0600

    completed the card.jsx and added in local states

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,26 @@ import './App.css'
 import Footer from './components/Footer/Footer'
 import Card from './components/Card/Card'
 import Header from './components/Header/Header'
+import { useState } from 'react'
 
 // Main app function where components are inserted
 function App() {
+    const [globalName, setGlobalName] = useState('Default');
+
+    const globalNameChange = () => {
+        setGlobalName("GLOBAL NAME");
+    }
 
     return (
         <>
         <Header />
-        <Card />
+
+        <button onClick={globalNameChange}>Change All Names</button>
+
+        <Card globalName={globalName}/>
+        <Card globalName={globalName}/>
+        <Card globalName={globalName}/>
+
         <Footer />
         </>
     )
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,26 @@
-function Card() {
+//  To use states, you need hooks
+//  To identify a hook inside of react
+//  {} represent all of react - react is a big object
+// Which is the same as
+// import React from 'react'
+// const variable = React.useState();
+import { useState } from 'react';
+
+function Card(props) {
+    // Use state function gives us back an array with 2 properties - kittykat names
+    // React has determined they should be as followed
+    const [ myName, setMyName] = useState('Initial Value - Default');
+
+    function changeName() {
+        // Goal of function it to set name to new name
+        setMyName('Ryan'); // Whatever is passed between the () is the new value for myName
+    }
+
     return (
         <div className="card">
             <button>count is 0</button>
+            <p>{props.globalName ? props.globalName : myName}</p>
+            <button onClick={changeName}>Change Name</button>
             <p>
             Edit <code>src/App.jsx</code> and save to test HMR. BAM
             </p>
