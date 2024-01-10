69a78b6a7cfdfe7d01e198000b255597d5e5e0c0
Ryan Barfield <cadium828@gmail.com>
2024-01-09 20:28:07 -0600

    added the header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,15 @@
 // Import modules
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Footer from './components/Footer/Footer'
 import Card from './components/Card/Card'
+import Header from './components/Header/Header'
 
 // Main app function where components are inserted
 function App() {
-    const [count, setCount] = useState(0)
 
     return (
         <>
-        <div>
-            <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
-        </div>
-
-        <h1>Vite + React</h1>
+        <Header />
         <Card />
         <Footer />
         </>
